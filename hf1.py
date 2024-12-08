ccf7d5087e2ba5b93e2955cab081ecd1b622db0b
dhanushpittala11 <108401638+dhanushpittala11@users.noreply.github.com>
2024-12-08 11:24:36 +0530

    Update hf1.py

--- hf1.py
+++ hf1.py
@@ -1,1 +1,63 @@
+import sagemaker
+import boto3
+sess = sagemaker.Session()
+# sagemaker session bucket -> used for uploading data, models and logs
+# sagemaker will automatically create this bucket if it not exists
+sagemaker_session_bucket=None
+if sagemaker_session_bucket is None and sess is not None:
+    # set to default bucket if a bucket name is not given
+    sagemaker_session_bucket = sess.default_bucket()
 
+try:
+    role = sagemaker.get_execution_role()
+except ValueError:
+    iam = boto3.client('iam')
+    role = iam.get_role(RoleName='sagemaker_execution_role')['Role']['Arn']
+
+sess = sagemaker.Session(default_bucket=sagemaker_session_bucket)
+
+print(f"sagemaker role arn: {role}")
+print(f"sagemaker session region: {sess.boto_region_name}")
+
+from sagemaker.huggingface.model import HuggingFaceModel
+
+# Hub model configuration <https://huggingface.co/models>
+hub = {
+  'HF_MODEL_ID':'distilbert-base-uncased-distilled-squad', # model_id from hf.co/models
+  'HF_TASK':'question-answering'                           # NLP task you want to use for predictions
+}
+
+# create Hugging Face Model Class
+huggingface_model = HuggingFaceModel(
+   env=hub,                                                # configuration for loading model from Hub
+   role=role,                                              # IAM role with permissions to create an endpoint
+   transformers_version="4.26",                             # Transformers version used
+   pytorch_version="1.13",                                  # PyTorch version used
+   py_version='py39',                                      # Python version used
+)
+
+# deploy model to SageMaker Inference
+predictor = huggingface_model.deploy(
+   initial_instance_count=1,
+   instance_type="ml.m5.xlarge"
+)
+
+# example request: you always need to define "inputs"
+data = {
+"inputs": {
+	"question": "What is used for inference?",
+	"context": "My Name is Philipp and I live in Nuremberg. This model is used with sagemaker for inference."
+	}
+}
+
+# request
+predictor.predict(data)
+
+data = {
+"inputs": {
+	"question": "How many indeginious territories exist in amazon rainforest?",
+	"context": "The Amazon rainforest,also called Amazon jungle or Amazonia, is a moist broadleaf tropical rainforest in the Amazon biome that covers most of the Amazon basin of South America. This basin encompasses 7,000,000 km2 (2,700,000 sq mi), of which 6,000,000 km2 (2,300,000 sq mi) are covered by the rainforest. This region includes territory belonging to nine nations and 3,344 indigenous territories."
+	}
+}
+
+predictor.predict(data)
